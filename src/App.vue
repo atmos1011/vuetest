67caa0c50a0e5782a4eb89cbca9dca15b95156e1
atmos1011 <vyxel154@gmail.com>
2026-08-10 16:01:17 +0700

    take note in detail here

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <nav style="margin-bottom: 20px;">
+      <router-link to="/" style="margin-right: 10px;">Home</router-link>
+      <router-link to="/table">My static table</router-link>
+    </nav>
+
+    <router-view />
+  </div>
+</template>
